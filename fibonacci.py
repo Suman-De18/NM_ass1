023b95a32c3d09f416db8064d1b1da1800effd6b
Suman-De18 <sumande191@gmail.com>
2023-11-17 17:00:15 +0530

    Python module that contains a function to generate n Fibonacci numbers

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,7 @@
-def fibonacci_sequence(n):
+# We can convert the previous code into a Python module that contains a function to generate n Fibonacci numbers:
+def generate_fibonacci(n):
     fib_sequence = [0, 1]
     for i in range(2, n):
         next_num = fib_sequence[-1] + fib_sequence[-2]
         fib_sequence.append(next_num)
     return fib_sequence[:n]
-
-# Print the first ten Fibonacci numbers
-first_ten = fibonacci_sequence(10)
-print("The first ten Fibonacci numbers are:", first_ten)
