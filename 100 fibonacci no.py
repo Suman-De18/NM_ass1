529aea6f9f96b9b23877c283a6a9edc0a2294ead
Suman-De18 <sumande191@gmail.com>
2023-11-17 17:34:52 +0530

    measuring the time taken to generate 100 Fibonacci numbers

--- 100 fibonacci no.py
+++ 100 fibonacci no.py
@@ -1,5 +1,6 @@
+import timeit
 import fibonacci
-#printing the first 100 Fibonacci numbers
-n = 100
-fib_numbers = fibonacci.generate_fibonacci(n)
-print(f"The first {n} Fibonacci numbers are: {fib_numbers}")
+
+# Measure the time taken to generate 100 Fibonacci numbers
+execution_time = timeit.timeit('fibonacci.generate_fibonacci(100)', setup='import fibonacci', number=1)
+print(f"Time taken to generate 100 Fibonacci numbers: {execution_time:.6f} seconds")
